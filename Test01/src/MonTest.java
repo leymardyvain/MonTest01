7bb97a1f2cff5c94a9353a5bb8e6ce87e4d67318
Yvain Leymard <Yvain Leymard@DESKTOP-LV5N9BE>
2025-01-09 11:34:07 +0100

    Forth Commit

--- Test01/src/MonTest.java
+++ Test01/src/MonTest.java
@@ -2,7 +2,7 @@
 public class MonTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello mon test, est ce que ça marche ?");
+		System.out.println("Hello mon test, est ce que ca marche ?");
 		System.out.println("c'est mon test");
 	}
 
